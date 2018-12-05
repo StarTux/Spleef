4a3e34680a09e235d5651c3f3255b552677d734a
StarTux <ths.loy@googlemail.com>
2018-12-05 12:57:20 +0100

    connect namespace

--- src/main/java/com/winthier/minigames/spleef/Spleef.java
+++ src/main/java/com/winthier/minigames/spleef/Spleef.java
@@ -2,7 +2,7 @@ package com.winthier.minigames.spleef;
 
 import com.winthier.connect.Connect;
 import com.winthier.connect.Message;
-import com.winthier.connect.bukkit.event.ConnectMessageEvent;
+import com.winthier.connect.event.ConnectMessageEvent;
 import java.io.FileReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
